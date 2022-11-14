f7508a2e3441f6f141c722abad526742a89cf242
minaminao <minaminaoy@gmail.com>
2022-11-14 10:07:49 +0900

    Add .SpaceVim.d/init.toml

--- install.sh
+++ install.sh
@@ -10,14 +10,8 @@ if [ "$(uname)" == "Linux" ]; then
   curl -sLf https://spacevim.org/install.sh | bash
 fi
 
-# Vim のプラグインマネージャー vim-plug をインストール
-# https://github.com/junegunn/vim-plug
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-      https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-
 # dotfile をコピー
-for f in .??*
-do
+for f in .??*; do
   # このリポジトリの.gitはスキップ
   if [ "$f" = ".git" ]; then
     continue
@@ -26,31 +20,38 @@ do
   if [ "$f" = ".vimrc" ]; then
     continue
   fi
-  # .bashrc は直接編集しない
+  # .bashrcは直接編集しない
   if [ "$f" = ".bashrc" ]; then
     ln -snfv "$PWD"/".bashrc" "$HOME"/".bashrc.dotfiles"
     # 一度編集すれば良い
     if [ -z "$DOTFILES_BASHRC_INSTALLED" ]; then
-      echo 'export DOTFILES_BASHRC_INSTALLED=1' >> ~/.bashrc
-      echo 'source ~/.bashrc.dotfiles' >> ~/.bashrc
+      echo 'export DOTFILES_BASHRC_INSTALLED=1' >>~/.bashrc
+      echo 'source ~/.bashrc.dotfiles' >>~/.bashrc
       source ~/.bashrc
     fi
     continue
   fi
-  # .bash_profile は直接編集しない
+  # .bash_profileは直接編集しない
   if [ "$f" = ".bash_profile" ]; then
     ln -snfv "$PWD"/".bash_profile" "$HOME"/".bash_profile.dotfiles"
     # IS_EDITTED: .bash_profile を編集したかどうか
     if [ -z "$DOTFILES_BASHPROFILE_INSTALLED" ]; then
-      echo 'export DOTFILES_BASHPROFILE_INSTALLED=1' >> ~/.bash_profile
-      echo 'source ~/.bash_profile.dotfiles' >> ~/.bash_profile
+      echo 'export DOTFILES_BASHPROFILE_INSTALLED=1' >>~/.bash_profile
+      echo 'source ~/.bash_profile.dotfiles' >>~/.bash_profile
       source ~/.bash_profile
     fi
     continue
   fi
-  ln -snfv "$PWD"/"$f" "$HOME"/"$f" 
+  if [ "$f" = ".config" ]; then
+    continue
+  fi
+  if [ "$f" = ".SpaceVim.d" ]; then
+    continue
+  fi
+  ln -snfv "$PWD"/"$f" "$HOME"/"$f"
 done
 
 ln -snfv "$PWD"/".config/fish/config.fish" "$HOME"/".config/fish/config.fish"
 ln -snfv "$PWD"/".config/alacritty/alacritty.yml" "$HOME"/".config/alacritty/alacritty.yml"
 ln -snfv "$PWD"/".config/starship.toml" "$HOME"/".config/starship.toml"
+ln -snfv "$PWD"/".SpaceVim.d/init.toml" "$HOME"/".SpaceVim.d/init.toml"
